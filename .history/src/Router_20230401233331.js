811c13dc574ed73afeee5accdb242c3ceec19103
falkon2 <94387470+falkon2@users.noreply.github.com>
2023-04-01 23:39:11 +0530

    merge conflict/WIP Dashboard

---
+++ .history/src/Router_20230401233331.js
@@ -0,0 +1,28 @@
+import {Routes, Route} from 'react-router-dom';
+import AdminDashboardPage from './pages/AdminDashboardPage';
+import StudentDashboardPage from './pages/StudentDashboardPage';
+import Welcome from './Welcome'
+import AdminLoginPage from './pages/AdminLoginPage'
+import QuizPage from './pages/QuizFormPage';
+import TestView from './pages/TestView';
+import PreviewQuiz from "./components/QuizPreview"
+import EditQuiz from "./components/EditQuiz"
+import NotFound from './pages/NotFound';
+function Router() {
+    return (
+        <Routes>
+            <Route path='*' element={<NotFound />}/>
+            <Route path="/" element={<Welcome/>} />
+            <Route path="/Login" element={<AdminLoginPage/>} />
+            <Route path="/admin-dashboard" element={<AdminDashboardPage/>} />
+            <Route path="/admin-dashboard/test-view" element={<TestView />} />
+            <Route path="/user-dashboard" element={<StudentDashboardPage/>} />
+            <Route path="/create-quiz" element={<QuizPage/>} />
+            <Route path="/prev-quiz" element={<PreviewQuiz/>} />
+            <Route path="/edit-quiz" element={<EditQuiz/>} />
+
+        </Routes>
+    )
+}
+
+export default Router;
