315a41fd93c5386b257a1ad2fb53ab108a27e35f
falkon2 <eufalkon@gmail.com>
2023-12-12 17:23:23 +0530

    new npm version and speed insights vercel

--- src/index.jsx
+++ src/index.jsx
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <SpeedInsights/>
             <Router/>
             <Analytics/>
+            <SpeedInsights/>
         </BrowserRouter>
     </React.StrictMode>
 );
