b3a5867784c556d5577369bde21f3c18a43ef9d4
falkon2 <eufalkon@gmail.com>
2023-12-12 17:19:35 +0530

    new npm version and speed insights vercel

--- src/index.jsx
+++ src/index.jsx
@@ -3,18 +3,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/globalStyles.css';
 import Router from './Router';
-import { BrowserRouter } from 'react-router-dom';
-import { Analytics } from '@vercel/analytics/react';
-
+import {BrowserRouter} from 'react-router-dom';
+import {Analytics} from '@vercel/analytics/react';
+import {SpeedInsights} from "@vercel/speed-insights/next"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Router />
-      <Analytics />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <SpeedInsights/>
+            <Router/>
+            <Analytics/>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 
